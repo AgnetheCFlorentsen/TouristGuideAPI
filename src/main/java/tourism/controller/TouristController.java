a8c37f7f526348b50fb621f4cbeeec0390fa4295
Agnethe Florentsen <agfl0001@stud.kea.dk>
2024-02-06 11:48:54 +0100

    1

---
+++ src/main/java/tourism/controller/TouristController.java
@@ -0,0 +1,29 @@
+package tourism.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tourism.model.TouristAttraction;
+import tourism.service.TouristService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("attractions")
+public class TouristController {
+
+    private TouristService touristService;
+    public TouristController(){
+        this.touristService = new TouristService();
+    }
+
+    @GetMapping(path = "")
+    public ResponseEntity<List<TouristAttraction>> getTouristAttractions() {
+        List attractions = touristService.getTouristAttrations();
+        return new ResponseEntity<List<TouristAttraction>>(attractions, HttpStatus.OK);
+    }
+
+
+}
--- src/main/java/tourism/service/TouristService.java
+++ src/main/java/tourism/service/TouristService.java
@@ -1,4 +1,22 @@
 package tourism.service;
 
+import tourism.model.TouristAttraction;
+import tourism.repository.TouristRepository;
+
+import java.util.List;
+
 public class TouristService {
+    private TouristRepository repository;
+    public TouristService () {
+        repository = new TouristRepository();
+    }
+    public List <TouristAttraction> getTouristAttrations (){
+        return repository.getTouristAttractions();
+    }
+
+    public TouristAttraction getTouristAttraction (String name) {
+        return repository.getTouristAttraction(name);
+    }
+
+
 }
