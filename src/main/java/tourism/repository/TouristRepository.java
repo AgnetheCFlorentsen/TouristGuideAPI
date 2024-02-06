4a891e11574e36cc55b88674321a63db51279a62
Agnethe Florentsen <agfl0001@stud.kea.dk>
2024-02-06 11:48:54 +0100

    1

--- src/main/java/tourism/repository/TouristRepository.java
+++ src/main/java/tourism/repository/TouristRepository.java
@@ -1,7 +1,37 @@
 package tourism.repository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import tourism.model.TouristAttraction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class TouristRepository {
+    private ArrayList<TouristAttraction> attractions;
+
+    public TouristRepository(){
+        this.attractions = new ArrayList<>();
+        attractions.add(new TouristAttraction("Guldbar", "Fredagscafé"));
+        attractions.add(new TouristAttraction("Hatten", "God bar"));
+        attractions.add(new TouristAttraction("Bip bip bar", "Dyr bar"));
+    }
+
+public List<TouristAttraction> getTouristAttractions(){
+        return attractions;
+}
+
+public TouristAttraction getTouristAttraction (String name){
+        for (TouristAttraction t : attractions){
+            if (name.equals(t.getName())){
+                return t;
+            }
+        }
+        return null;
+}
+
+
 }
